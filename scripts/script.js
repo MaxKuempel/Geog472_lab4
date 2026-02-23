12e4bb5a341ef6a42e12b0b0d5949c80f33c33fe
MaxKuempel <kuempema@oregonstate.edu>
2026-02-23 00:57:58 -0800

    working mapbox style, hovering

--- scripts/script.js
+++ scripts/script.js
@@ -22,11 +22,19 @@ function FetchPorts(){
         }
     )
 }
-var map = L.map('map').setView([0, 0], 2);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+//var map = L.map('map').setView([0, 0], 2)
+
+var map = L.map('map')
+        .setView([0, 0], 2)
+
+L.tileLayer(
+            'https://api.mapbox.com/styles/v1/maxkuempel/cmlywfg2x001a01smhezzavbb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF4a3VlbXBlbCIsImEiOiJjbWxmaTRnemgwMjhpM2VxNHY0OWgyZnowIn0.4vT-IDEnXFLYMUFluo4MSw',
+            {
+              maxZoom: 19,
+              attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> © <a href="https://www.mapbox.com/about/maps/">Mapbox</a> <strong><a href="https://labs.mapbox.com/contribute/" target="_blank">Improve this map</a></strong>'
+            }
+          ).addTo(map);
 
 var fromport ;
 
@@ -36,10 +44,12 @@ var linestyle = {
 
 var portflows_render;
 function DisplayPortFlows(fromport){
-    
    portflows_render =  L.geoJSON(portflows, {
         style: function(feature){
-            return {weight: feature.properties.FLOWS / 50000 };
+            return {weight: feature.properties.FLOWS / 50000,
+                color:  "#f7ea45"
+            };
+            
         },
         filter: function(feature,layer){
             return feature.properties.Port1 == fromport;
@@ -49,26 +59,38 @@ function DisplayPortFlows(fromport){
 }   
 
 function DisplayPorts(){
+    
     L.geoJSON(ports, {
+        onEachFeature: function (feature, layer){
+            layer.bindTooltip(
+                feature.properties.Name +
+                ", " +
+                feature.properties.Country)
+        },
         pointToLayer: function(feature, coords){
             markerOptions = {
-                radius: 3,
+                radius: 8,
                 weight: 1,
-                color: "#000000",
-                FillColor: "#EEF527"
+                color: 	"#bfc05b",
             };
             return L.circleMarker(coords, markerOptions);
         }
-    }).addTo(map).on('mouseover', onHover).on('mouseout', ClearLines);
+    }).addTo(
+        map
+    ).on('mouseover', onHover).on('mouseout', ClearLines);
 }
 
-function ClearLines(){
-    console.log("mouse off")
+
+function ClearLines(e){
+    console.log("mouse off :(")
     portflows_render.clearLayers();
+    this.closePopup();
 }
 function onHover(e){
+    console.log("mouse on!");
     fromport = e.layer.feature.properties.LOCODE;
     DisplayPortFlows(fromport);
+    this.openPopup();
 }
 
 window.onload = function(){
